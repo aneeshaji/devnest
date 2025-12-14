afa5551da0e31d49c19467a78ca820cfc94151fe
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2025-12-14 12:26:51 +0530

    Configuration Updates For AWS EC2

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+	origin: ['http://localhost:3000', ''], // Todo - Add S3 URL
+	credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
